d31af33a70f158c705af2e0b9f9823afca8119a6
Dominic Tarr <dominic.tarr@gmail.com>
2018-10-03 23:35:59 +1300

    working again

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ exports.permissions = {
 }
 
 exports.init = function (sbot, config) {
+  console.log("CONFIG", config)
   var endpoints = {}
   var portal = config.tunnel && config.tunnel.portal
 
@@ -39,74 +40,91 @@ exports.init = function (sbot, config) {
       opts = string
     else {
       var parts = string.split(':')
-      var portal = parts.slice(3).join(':')
+      if(parts[0] != 'tunnel') return
       opts = {
-        name: 'tunnel',
-        id: parts[1],
-        port: +parts[2],
-        portal: portal
+        name: parts[0],
+        portal: parts[1],
+        target: parts[2],
+        port: +parts[3] || 0,
       }
     }
 
     if(!(
       opts.name === 'tunnel' &&
-      isFeed(opts.id) &&
-      Number.isInteger(opts.port) &&
-      sbot.multiserver.parse(opts.portal)
-    )) throw new Error('weird:'+string)
+      isFeed(opts.portal) &&
+      isFeed(opts.target) &&
+      Number.isInteger(opts.port)
+    )) return
 
     return opts
   }
 
   var handlers = {}
 
-  sbot.multiserver.transport(function (instance) {
-    return {
-      name: 'tunnel',
-      server: function (onConnect) {
-        //just remember the reference, call it
-        //when the tunnel api is called.
-        handlers[instance] = onConnect
-      },
-      client: function (addr, cb) {
-        var opts = parse(addr)
-        return Client(sbot.keys, {
-          remote: opts.portal,
-          caps: config.caps,
-          manifest: sbot.getManifest()
-        }, function (err, rpc) {
-          if(err) cb(err)
-          else cb(null, rpc.tunnel.connect({id: opts.id, port: opts.port}))
-        })
-      },
-      parse: parse,
-      stringify: function () {
-        if(portal)
-          return ['tunnel', sbot.id, instance, portal].join(':')
+  sbot.multiserver.transport({
+    name: 'tunnel',
+    create: function (config, instance) {
+      instance = instance || 0
+      console.log("CREATE MS PLUGIN, TUNNEL")
+      return {
+        name: 'tunnel',
+        scope: function () { return config.scope || 'public' },
+        server: function (onConnect) {
+          //just remember the reference, call it
+          //when the tunnel api is called.
+          handlers[instance] = onConnect
+          console.log("CREATED TUNNEL SERVER:", sbot.id)
+        },
+        client: function (addr, cb) {
+          var opts = parse(addr)
+          console.log("TUNNEL OPTS", opts)
+          sbot.gossip.connect(opts.portal, function (err, rpc) {
+            if(err) cb(err)
+            else cb(null, rpc.tunnel.connect({target: opts.target, port: opts.port}))
+          })
+        },
+        parse: parse,
+        stringify: function () {
+          if(portal)
+            return ['tunnel', portal, sbot.id, instance].join(':')
+        }
       }
     }
   })
 
   setImmediate(function () {
-    if(config.tunnel && config.tunnel.portal)
-      sbot.connect(config.tunnel.portal, function (err, rpc) {
+    if(config.tunnel && config.tunnel.portal) {
+      console.log("CONNECT TO PORTAL", config.tunnel)
+      console.log('PORTAL', sbot.gossip.get(config.tunnel.portal))
+      var addr = sbot.gossip.get(config.tunnel.portal).address
+      sbot.gossip.connect(config.tunnel.portal, function (err, rpc) {
+        if(err) {
+          return console.error('failed to connect to portal:',config.tunnel.portal)
+          console.error(err.stack)
+        }
         rpc.tunnel.announce(null, function () {
-
+          console.log("ANNOUNCED:", sbot.id, 'at', config.tunnel.portal)
         })
       })
+    }
   })
 
   return {
     announce: function (opts) {
+      console.log('announce', this.id, opts)
       endpoints[this.id] = sbot.peers[this.id][0]
     },
     connect: function (opts, cb) {
-      if(endpoints[opts.id]) {
-        return endpoints[opts.id].tunnel.connect(opts, function (err) {
+      console.log("TUNNEL CONNECT", opts, {from: this.id, to: opts})
+      //if we are being asked to forward connections...
+      //TODO: config to disable forwarding
+      if(endpoints[opts.target]) {
+        return endpoints[opts.target].tunnel.connect(opts, function (err) {
           if(cb) cb(err)
         })
       }
-      else if(opts.id === sbot.id && handlers[opts.port]) {
+      //if this connection is for us
+      else if(opts.target === sbot.id && handlers[opts.port]) {
         var streams = DuplexPair()
         handlers[opts.port](streams[0])
         return streams[1]
@@ -116,4 +134,14 @@ exports.init = function (sbot, config) {
     }
   }
 }
+
+
+
+
+
+
+
+
+
+
 
